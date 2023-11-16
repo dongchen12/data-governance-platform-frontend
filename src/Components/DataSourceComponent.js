a828b6f801740008683104679ea38222ec3df19e
Xu Guohong <xgh127@sjtu.edu.cn>
2023-11-16 22:27:30 +0800

    删掉fieldMap，增加测试连接后数据表的勾选，点击按钮可以查看选过的数据表

--- src/Components/DataSourceComponent.js
+++ src/Components/DataSourceComponent.js
@@ -1,15 +1,10 @@
 import React, {useState} from 'react';
-import {Button, Table, Modal, Form, Input, Space, Tag, Checkbox} from 'antd';
-import { MinusCircleOutlined, PlusOutlined } from '@ant-design/icons';
+import {Button, Checkbox, Form, Input, Modal, Popover, Select, Space, Table, Tag} from 'antd';
 
 const DataSourceComponent = () => {
     const [dataSourceList, setDataSourceList] = useState([]);
     const [dataSourceFormVisible, setDataSourceFormVisible] = useState(false);
     const [form] = Form.useForm();
-    const [fieldMapModalVisible, setFieldMapModalVisible] = useState(false);
-    const [currentDataSource, setCurrentDataSource] = useState(null);
-    const [fieldMapForm] = Form.useForm();
-// 在数据源对象中初始化 fieldMap 为一个空数组
     const initialDataSource = {
         sourceType: '',
         dataSource: {
@@ -17,42 +12,18 @@ const DataSourceComponent = () => {
             url: '',
             tag: [],
             description: '',
+            tables: [],
         },
-        //tables嵌套，里面是一个数组，每个元素有tableName属性还有fieldMap属性
-        tables: [],
     };
     const handleDataSourceFormSubmit = () => {
         form.validateFields().then((values) => {
             // 来自 initialDataSource 和 values 的值被合并到一个新对象 (newDataSource) 中，然后将这个新对象添加到 dataSourceList
-            alert(JSON.stringify(values));
+            //将values中的tables属性的值添加到initialDataSource中的dataSource属性中的tables属性中
             setDataSourceList([...dataSourceList, { ...initialDataSource, ...values }]);
             form.resetFields();
             setDataSourceFormVisible(false);
         });
 
-    };
-
-    //TODO:修改下面的函数，使得其能更新对应table的fieldMap，而不是更新dataSource的fieldMap
-    const handleFieldMapFormSubmit = () => {
-        fieldMapForm.validateFields().then((values) => {
-            const updatedDataSourceList = dataSourceList.map((dataSource) => {
-                // 寻找当前表格所在的数据源
-                const foundTable = dataSource.tables.find((table) => table === currentDataSource);
-                if (foundTable) {
-                    // 更新当前表格的 fieldMap 值为 values.fieldMap
-                    const updatedTables = dataSource.tables.map((table) => {
-                        if (table === foundTable) {
-                            return { ...table, fieldMap: values.fieldMap };
-                        }
-                        return table;
-                    });
-                    return { ...dataSource, tables: updatedTables };
-                }
-                return dataSource;
-            });
-            setDataSourceList(updatedDataSourceList);
-            setFieldMapModalVisible(false);
-        });
 
     };
     //处理删除数据源的函数
@@ -97,6 +68,16 @@ const DataSourceComponent = () => {
             key: 'actions',
             render: (text, record) => (
                 <Space>
+                    <Popover content={
+                        <ul>
+                            {record.dataSource.tables.map((table) => (
+                                <li key={table.tableId}>{table.tableName}</li>
+                            ))}
+                        </ul>
+
+                    } title="选中的数据表" trigger="click">
+                        <Button type='primary'>查看数据表</Button>
+                    </Popover>
                     <Button
                         //删除数据源,将type设置为delete
                         danger
@@ -110,8 +91,6 @@ const DataSourceComponent = () => {
                                     onOk: () => {
                                         handleDeleteDataSource(record);
                                     },
-
-
                                 }
                             );
 
@@ -128,7 +107,6 @@ const DataSourceComponent = () => {
         try {
             // 模拟数据库连接信息，实际使用时需要替换为真实的连接信息
             const databaseInfo = {
-                type: form.getFieldValue(['dataSource', 'type']),
                 url: form.getFieldValue(['dataSource', 'url']),
                 username: form.getFieldValue('username'),
                 password: form.getFieldValue('password'),
@@ -137,40 +115,46 @@ const DataSourceComponent = () => {
             // 调用测试数据库连接的函数
             // await testDatabaseConnection(databaseInfo);
 
-            // 这里可以进行连接成功后的处理，例如提示、更新UI等
-
-            const TestTables = [{
-                tableId: 1,
-                tableName: 'table1',
-                fieldMap: [],
-                checked: false,
-            }, {
-                tableId: 2,
-                tableName: 'table2',
-                fieldMap: [],
-                checked: false,
-            }, {
-                tableId: 3,
-                tableName: 'table3',
-                fieldMap: [],
-                checked: false,
-            }];
-            // 将测试结果添加到表单的 dataSource下的 tables 中
-            form.setFieldsValue({ 'tables': TestTables });
+            const TestTables = [
+                { tableId: 1, tableName: 'table1', checked: false },
+                { tableId: 2, tableName: 'table2', checked: false },
+                { tableId: 3, tableName: 'table3', checked: false },
+            ];
+            // 连接成功后，展示连接成功的提示信息，然后将数据库中的表信息展示出来，并且让用户勾选需要的表
+            // 创建一个模态框并用于显示成功提示和表信息选择
             Modal.success({
                 title: '数据库连接成功',
                 content: (
                     <div>
-                        <p>数据库中的表：</p>
-                        <p>{JSON.stringify(databaseInfo)}</p>
-                        <ul>
-                            {TestTables.map((table) => (
-                                <li key={table.tableId}>{table.tableName}</li>
-                            ))}
-                        </ul>
-                    </div>
+                        <h1>连接成功！</h1>
 
+                        <p>选择需要的表：</p>
+                        <Checkbox.Group style={{ width: '100%' }}>
+                            {TestTables.map(table => (
+                                <Checkbox key={table.tableId} value={table.tableName}
+                                onChange={(e) => {
+                                    table.checked = e.target.checked;
+                                }
+                                }
+                                >
+                                    {table.tableName}
+                                </Checkbox>
+                            ))}
+                        </Checkbox.Group>
+                    </div>
                 ),
+                onOk: () => {
+                    const selectedTables = TestTables.filter(table => table.checked).map(table => table);
+                    //将表格信息添加到initialDataSource中的dataSource属性中的tables属性中
+                    form.setFieldsValue({ dataSource: { tables: selectedTables } });
+                    Modal.success({
+                        title: '数据表添加成功',
+                        okText: '确认',
+
+                    });
+
+                },
+                okText: '确认',
 
             });
 
@@ -184,69 +168,7 @@ const DataSourceComponent = () => {
             });
         }
     };
-    const expandedRowRender = (record) => {
-        const tableColumns = [
-            {
-                title: 'Table Name',
-                dataIndex: 'tableName',
-                key: 'tableName',
-                render: (text, tableRecord) => (
-                    <Checkbox
-                        checked={tableRecord.checked}
-                        //实现点击checkbox选中表格
-                        onChange={(e) => {  //e是事件对象
-                            const checked = e.target.checked;//获取checkbox的选中状态
-                            //更新dataSourceList
-                            const updatedDataSourceList = dataSourceList.map((dataSource) => {
-                                //如果dataSource是当前的dataSource
-                                if (dataSource === record) {
-                                    //更新tables
-                                    const updatedTables = dataSource.tables.map((table) => {
-                                        if (table === tableRecord) {
-                                            return { ...table, checked };
-                                        }
-                                        return table;
-                                    });
-                                    return { ...dataSource, tables: updatedTables };
-                                }
-                                return dataSource;
-});
-                            setDataSourceList(updatedDataSourceList);
-                        }}
-                    >
-                        {text}
-                    </Checkbox>
-                ),
-            },
 
-            {
-                title: 'Actions',
-                key: 'actions',
-                render: (text, record) => (
-                    <Space>
-                        <Button
-                            disabled={!record.checked}
-                            type="primary"
-                            onClick={() => {
-                                // 保存当前数据源对象
-                                setCurrentDataSource(record);
-                                // 将当前数据源对象的 fieldMap 值设置到 fieldMapForm 中
-                                fieldMapForm.setFieldsValue({ fieldMap: record.fieldMap || {} });
-                                // 显示编辑映射的对话框
-                                setFieldMapModalVisible(true);
-                            }}
-                        >
-                            编辑映射
-                        </Button>
-                    </Space>
-                ),
-            },
-
-        ];
-        const dataSourceTables = record.tables || [];
-        // const dataSourceTables = record.dataSource.tables || [];
-        return <Table columns={tableColumns} dataSource={dataSourceTables} pagination={false} />;
-    };
     return (
         <div>
             <Button type="primary" onClick={() => setDataSourceFormVisible(true)}>
@@ -255,7 +177,7 @@ const DataSourceComponent = () => {
             <Table
                 dataSource={dataSourceList}
                 columns={columns}
-                expandable={{ expandedRowRender }}/>
+                />
 
             <Modal
                 title="添加数据源"
@@ -271,7 +193,23 @@ const DataSourceComponent = () => {
                         <Input />
                     </Form.Item>
                     <Form.Item name={['dataSource', 'type']} label="Data Source Type" rules={[{ required: true, message: 'Please input Data Source Type' }]}>
-                        <Input />
+                        <Select
+                            placeholder="请选择数据源类型"
+                            initialValue={'text'}
+                            options={[
+                                {
+                                    value: 'seq',
+                                    label: '时序',
+                                },
+                                {
+                                    value: 'image',
+                                    label: '图像',
+                                },
+                                {
+                                    value: 'text',
+                                    label: '文本',
+                                },
+                            ]}/>
                     </Form.Item>
                     <Form.Item name={['dataSource', 'url']} label="URL" rules={[{ required: true, message: 'Please input URL' }]}>
                         <Input />
@@ -287,7 +225,7 @@ const DataSourceComponent = () => {
                             测试数据库连接
                         </Button>
                     </Form.Item>
-                    <Form.Item name='tables' hidden>
+                    <Form.Item name={['dataSource','tables']} hidden>
                         <Input />
                     </Form.Item>
                     <Form.Item name={['dataSource', 'tag']} label="Tag">
@@ -296,55 +234,6 @@ const DataSourceComponent = () => {
                     <Form.Item name={['dataSource', 'description']} label="Description" rules={[{ required: true, message: 'Please input Description' }]}>
                         <Input />
                     </Form.Item>
-                </Form>
-            </Modal>
-
-            <Modal
-                title="编辑映射"
-                visible={fieldMapModalVisible}
-                onCancel={() => {
-                    fieldMapForm.resetFields();
-                    setFieldMapModalVisible(false);
-                }}
-                onOk={handleFieldMapFormSubmit}
-            >
-                <Form form={fieldMapForm}>
-                    <Form.List name="fieldMap">
-                        {(fields, { add, remove }) => (
-                            <>
-                                {fields.map(({ key, name, fieldKey, ...restField }) => (
-                                    <Space key={key} style={{ display: 'flex', marginBottom: 8 }}>
-                                        <Form.Item
-                                            {...restField}
-                                            name={[name, 'sourceColumn']}
-                                            fieldKey={[fieldKey, 'sourceColumn']}
-                                            label="Source Column"
-                                        >
-                                            <Input />
-                                        </Form.Item>
-                                        <Form.Item
-                                            {...restField}
-                                            name={[name, 'modelColumn']}
-                                            fieldKey={[fieldKey, 'modelColumn']}
-                                            label="Model Column"
-                                        >
-                                            <Input />
-                                        </Form.Item>
-                                        <MinusCircleOutlined onClick={() => remove(name)} />
-                                    </Space>
-                                ))}
-                                <Form.Item>
-                                    <Button
-                                        type="dashed"
-                                        onClick={() => add()}
-                                        icon={<PlusOutlined />}
-                                    >
-                                        添加映射
-                                    </Button>
-                                </Form.Item>
-                            </>
-                        )}
-                    </Form.List>
                 </Form>
             </Modal>
         </div>
